25c09b65eb85155e3fb8973f557046f12aff9af5
RodgersX <brianmawira2@gmail.com>
2023-01-08 11:31:01 +0300

    build for production

--- src/main.js
+++ src/main.js
@@ -7,9 +7,8 @@ import Sidenav from "./components/sidenav.vue";
 import axios from "axios";
 
 axios.defaults.withCredentials = false;
-axios.defaults.baseURL = "http://localhost:4000/api";
+axios.defaults.baseURL = "http://128.199.38.66:81/api";
 
-// 128.199.38.66:81
 // handle expired tokens
 axios.interceptors.response.use(undefined, function(err) {
   if(err) {
